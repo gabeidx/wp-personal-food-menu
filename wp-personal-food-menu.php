28688adb589aaf1f36b339cc285f325187040f05
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-03-16 18:11:35 -0300

    Add plugin main file with required info

---
+++ wp-personal-food-menu.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WP Personal Food Menu
+ * Plugin URI: https://github.com/gabrielizaias/wp-personal-food-menu
+ * Description: A plugin that calculates nutritional information based on foods selected by the user.
+ * Version: 0.1
+ * Author: Gabriel Izaias
+ * Author URI: http://gabrielizias.com
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ */
+?>
