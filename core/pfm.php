0b35a1307f2f953ea565148ad2abc5be03308a19
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-03-16 20:04:13 -0300

    Add plugin main class

---
+++ core/pfm.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* Main class
+*
+* PHP 5
+*
+* WP Personal Food Menu
+* Copyright 2013-2013, Mais Musculação. (http://www.maismusculacao.net)
+*
+* Licensed under The Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+* Redistributions of files must retain the above copyright notice.
+*
+* @package      WP Personal Food Menu
+* @copyright    Copyright 2013-2013, Mais Musculação.
+* @license      CC BY-NC-SA 3.0 License (http://creativecommons.org/licenses/by-nc-sa/3.0/)
+*/
+
+class Pfm {
+
+/**
+ * Plugin version
+ *
+ * @var string
+ */
+    public $version;
+
+/**
+ * Plugin options
+ *
+ * @var array
+ */
+    private $options;
+
+}
