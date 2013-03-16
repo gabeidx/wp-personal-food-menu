40b7a29948b481d96304964e706eae286c75114b
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-03-16 19:29:55 -0300

    Create constants with plugin paths (directory and url)

--- wp-personal-food-menu.php
+++ wp-personal-food-menu.php
@@ -8,4 +8,8 @@
  * Author URI: http://gabrielizias.com
  * License: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  */
+
+define('DS', DIRECTORY_SEPARATOR);
+define('PFM_DIR', plugin_dir_path(__FILE__));
+define('PFM_URL', plugin_dir_url(__FILE__));
 ?>
